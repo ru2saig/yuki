df58138a368730b68cce4ac66ff6213e084a2190
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-08-08 14:16:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+
+# Yuki
+
+An open-spource companion robot based on Rpi and Arduino. Has face recognition, mobility, eye animations to display current 
+"emotional state", has a mini speaker and LED's for displaying state, can follow a person and respond to pre-defined gestures, which can be programmed to turn on or off IOT-based devices over the Internet. The parts, code, designs are all open-sourced.
+
+## Getting Started
+
+Materials:
+```
+Materials go here porbably
+```
+
+Software:
+```
+software stuff and file names
+```
+
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Checking the final robot
+
+### Break down into end to end tests
+//probably won't need this section
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+//meh
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+//the tools and resources
+
+## Contributing
+
+//pull request? forking?
+
+## Interation
+//something like this to show the current iteration
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Nidhish** - *Designer,Programmer* - [Ruusaig](https://github.com/ru2saig)
+* **Anirban** - *Designer,Programmer* - [anirban-1009](https://github.com/anirban-1009)
+* **Ajay** - *Accounts,Designer,Moral Support* - [ajayteja-123](https://github.com/ajayteja-123)
+
+
+//maybe need this:
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+//need to find and add the correct Liscense
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+//uhhh....dedication?
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
+
---
+++ Yuki/main.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+
+#importing libraries
+import io
+import picamera
+import cv2
+import numpy
+import time
+from PIL import Image, ImageDraw, ImageSequence
+from google.cloud import vision
+from demo_opts import get_device
+from luma.core.sprite_system import framerate_regulator
+
+device = get_device()
+client = vision.ImageAnnotatorClient()
+
+#getting the harrcascade file
+faceCascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
+
+#blink function
+def blink():
+        regulator = framerate_regulator(fps=15)
+        source = Image.open('resources/fastblink.gif')
+        size = device.size
+
+        for frame in ImageSequence.Iterator(source):
+                with regulator:
+                        background = Image.new("RGB", device.size, "white")
+                        background.paste(frame)
+                        device.display(background.convert(device.mode))
+
+
+def wakeup():
+	print("Waking Yuki up")
+	image = Image.open('resources/wakeup0.png')
+	size = device.size
+	regulator = framerate_regulator(fps=7)
+
+	background = Image.new("RGB",device.size,"white")
+	image2 = Image.open('resources/wakeup1.gif')
+	image3 = Image.open('resources/wakeup2.gif')
+
+	#the eye line thing
+	background.paste(image)
+	device.display(background.convert(device.mode))
+	time.sleep(1.5)
+
+	#the slightly opened part
+	for frame in ImageSequence.Iterator(image2):
+		with regulator:
+			background = Image.new("RGB", device.size, "white")
+			background.paste(frame)
+			device.display(background.convert(device.mode))
+
+	time.sleep(2.5)
+
+	#the final part, opening the eyes fully
+
+	regulator = framerate_regulator(fps=20)
+	for frame in ImageSequence.Iterator(image3):
+		with regulator:
+			background = Image.new("RGB", device.size, "white")
+			background.paste(frame)
+			device.display(background.convert(device.mode))
+
+	time.sleep(2)
+
+#function to show joy
+def showjoy():
+	image = Image.open('resources/happy.gif')
+	size = device.size
+
+	regulator = framerate_regulator(fps=10)
+
+	for frame in ImageSequence.Iterator(image):
+		with regulator:
+			background = Image.new("RGB", device.size, "white")
+			background.paste(frame)
+			device.display(background.convert(device.mode))
+
+
+
+
+#function to avoid emotion
+def showavoid():
+	image = Image.open('resources/avoid.gif')
+	size = device.size
+	regulator = framerate_regulator(fps=10)
+	for frame in ImageSequence.Iterator(image):
+		with regulator:
+			background = Image.new("RGB", device.size, "white")
+			background.paste(frame)
+			device.display(background.convert(device.mode))
+
+#function for reverting to nuetral emotion
+def showneutral():
+	image = Image.open('resources/neutral.png')
+	size= device.size
+	background = Image.new("RGB",device.size,"white")
+	background.paste(image)
+	device.display(background.convert(device.mode))
+
+def sleep():
+	image0 = Image.open('resources/sleep.gif')
+	image1 = Image.open('resources/wakeup0.png')
+	size=device.size
+	regulator = framerate_regulator(fps=10)
+	for frame in ImageSequence.Iterator(image0):
+		with regulator:
+			background = Image.new("RGB", device.size, "white")
+			background.paste(frame)
+			device.display(background.convert(device.mode))
+
+
+	background.paste(image1)
+	device.display(background.convert(device.mode))
+	time.sleep(2)
+	print("Yuki wishes you a good sleep!")
+
+
+#function for taking a picture
+def snap():
+	with picamera.PiCamera() as camera:
+		camera.resolution = (640,480)
+		camera.capture(stream, format='jpeg')
+		print("click")
+
+wakeup()
+time.sleep(1)
+blink()
+#an output that signals the program start (replacement for the led)
+#need to add a stop case
+print("Starting main loop")
+
+while 1:
+
+	stream = io.BytesIO()
+	snap()
+	buff = numpy.frombuffer(stream.getvalue(), dtype=numpy.uint8)
+	img = cv2.imdecode(buff, 1)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+	key = cv2.waitKey(1) & 0xFF
+	if len(faces)>0:
+		print((faces[0][2]/2)+faces[0][0]) #abcissa of midpoint
+		print((faces[0][1]/2)+faces[0][3]) #ordinate of midpoint
+		cv2.imwrite('image.jpg',img) #saving the image for the API
+		with open('image.jpg', 'rb') as image_file: #emotion detection of the API
+                	content = image_file.read()
+		image = vision.types.Image(content=content)
+		response = client.face_detection(image=image)
+		faces = response.face_annotations
+		likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE','LIKELY', 'VERY_LIKELY')
+		for face in faces:
+			if likelihood_name[face.anger_likelihood] == 'LIKELY' or likelihood_name[face.anger_likelihood] == 'VERY_LIKELY':
+				showavoid()
+				time.sleep(3)
+				showneutral()
+			elif likelihood_name[face.joy_likelihood] =='LIKELY' or likelihood_name[face.joy_likelihood] =='VERY_LIKELY':
+				showjoy()
+				time.sleep(3)
+				showneutral()
+			elif likelihood_name[face.sorrow_likelihood] =='LIKELY' or likelihood_name[face.sorrow_likelihood] =='VERY_LIKELY':
+				showavoid()
+				time.sleep(3)
+				showneutral()
+			else:
+				blink()
+				showneutral()
+
+	if key == ord("q"):
+		break
+
+
+
+
+
+
+cap.release()
+cv2.destroyAllWindows()
+sleep()
+credential_path = "/path/to/creditionals" 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path 
